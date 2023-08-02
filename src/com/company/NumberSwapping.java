60fb9480ee5b31b9cefb1407b49586424fdedfb7
kayatk <thillaikayatk@gmail.com>
2023-08-02 23:23:53 +0530

    updating No Swaping exercise

---
+++ src/com/company/NumberSwapping.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class NumberSwapping {
+    public static void main(String[] args) {
+        //Method one
+        int i=10;
+        int j =20;
+        /*int c=i;
+        i=j;
+        j=c;
+        System.out.println(i +" "+ j);*/
+
+       //Method 2
+       /* i =i+j;
+        j=i-j;
+        i=i-j;
+        System.out.println(i +" "+ j);*/
+
+        //Method 3
+        i =i*j;
+        j=i/j;
+        i=i/j;
+        System.out.println(i +" "+ j);
+
+        //Method 4
+
+    }
+}
