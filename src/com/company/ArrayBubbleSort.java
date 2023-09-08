2c42254c7bd7bb82b859a8094d43e433577bd5dd
kayatk <thillaikayatk@gmail.com>
2023-09-08 13:40:12 +0530

    Sorting

---
+++ src/com/company/ArrayBubbleSort.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class ArrayBubbleSort {
+    public static void main(String args[]){
+        int no[]={1,7,8,10,3};
+        System.out.println("Before Sorting:"+ Arrays.toString(no));
+
+        int max=no[0];
+        for(int i=0;i<no.length;i++){
+            if(max>no[i]){
+
+            }
+        }
+
+    }
+}
